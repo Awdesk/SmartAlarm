d1776823d8a07f9151c9869935b89afaa8ccc901
Илья <87651970+Awdesk@users.noreply.github.com>
2024-04-13 15:52:04 +0700

    Update Parser.cs

--- Smart_Alarm/Parser.cs
+++ Smart_Alarm/Parser.cs
@@ -6,6 +6,21 @@ using System.IO;
 
 namespace Smart_Alarm
 {
+    public struct Lesson
+    {
+        public Lesson(string discipline, string auditoriums, string date, string time)
+        {
+            this.auditoriums = auditoriums;
+            this.time = time;
+            this.discipline = discipline;
+            this.data = date;
+        }
+        public string discipline;
+        public string auditoriums;
+        public string data;
+        public string time;
+    }
+
     internal class Parser
     {
         private readonly string url;
@@ -13,62 +28,32 @@ namespace Smart_Alarm
         public Parser(string groupID_Value, string faculties)
         {
             this.url = $"https://timetable.tusur.ru/faculties/{faculties}/groups/{groupID_Value}";
-            ParseTimetable();
         }
-        static private string __normalize_text(string text)
+        public List<Lesson> ParseTimetable()
         {
-            if (text != null)
-            {
-                string striped_text = text.Trim();
-                string replaced_text = striped_text.Replace(" ", "");
-                return replaced_text.Replace("\n", " ");
-            }
-            return null;
-        }
-
-        public List<Dictionary<string, object>> ParseTimetable()
-        {
-            List<Dictionary<string, object>> timetable = new List<Dictionary<string, object>>();
+            List<Lesson> lessons = new List<Lesson>();
             var doc = new HtmlWeb().Load(url);
+            var screen_reader_element = doc.DocumentNode.SelectSingleNode("//div[@class='timetable_wrapper']//table[@class='screen-reader-element']");
+            var days = screen_reader_element.SelectNodes("//tr").Where((num, index) => index % 2 == 0).ToList();
 
-            var table = doc.DocumentNode.SelectSingleNode("//table[@class='table']");
-            var thead = table.SelectSingleNode("thead");
-            var tbody = table.SelectSingleNode("tbody");
-            var days = thead.SelectNodes("tr/th").Skip(1).Select(d => __normalize_text(d.InnerText)).ToList();
-            var rows = tbody.SelectNodes("tr");
-
+            int count = 0;
             foreach (var day in days)
             {
-                List<Dictionary<string, object>> lessons = new List<Dictionary<string, object>>();
-                for (int i = 0; i < rows.Count; i++)
+                if (count < 7)
                 {
-                    var time = rows[i].SelectSingleNode("th[@class='time']");
-                    var lesson = rows[i].SelectNodes("td")[days.IndexOf(day)];
-                    var discipline = lesson.SelectSingleNode("span[@class='discipline']");
-                    var kind = lesson.SelectSingleNode("span[@class='kind']");
-                    var teacher = lesson.SelectSingleNode("span[@class='group']");
-
-                    string normalized_discipline = discipline != null ? __normalize_text(discipline.InnerText) : null;
-                    string normalized_kind = kind != null ? __normalize_text(kind.InnerText) : null;
-                    string normalized_teacher = teacher != null ? __normalize_text(teacher.InnerText) : null;
-                    string normalized_time = time != null ? __normalize_text(time.InnerText) : null;
-
-                    lessons.Add(new Dictionary<string, object>
-                {
-                    { "time", normalized_time },
-                    { "discipline", normalized_discipline },
-                    { "kind", normalized_kind },
-                    { "teacher", normalized_teacher }
-                });
+                    //var discipline = day.SelectNodes("//span[@class='discipline'][text()]").Where((stringa) => stringa != null).First().InnerText;
+                    var discipline = day.SelectSingleNode("//span[@class='discipline'][text()]").InnerText;
+                    var auditoriums = day.SelectSingleNode("//span[@class='auditoriums'][text()]").InnerText;
+                    string[] data = new string[] { day.SelectSingleNode("//div[@class='modal-body']/p[2][text()]").InnerText,
+                    day.SelectSingleNode("//div[@class='modal-body']/p[3][text()]").InnerText };
+                    Lesson newLesson = new Lesson(discipline, auditoriums, data[0], data[1]);
+                    lessons.Add(newLesson);
                 }
-                timetable.Add(new Dictionary<string, object>
-            {
-                { "day", day },
-                { "lessons", lessons }
-            });
+                else
+                    break;
+                ++count;
             }
-
-            return timetable;
+            return lessons;
         }
     }
 }
