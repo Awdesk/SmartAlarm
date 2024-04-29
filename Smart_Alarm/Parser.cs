a82bd1539b3d345e104e953a71ef008441ba981a
Илья <87651970+Awdesk@users.noreply.github.com>
2024-04-29 15:33:23 +0700

    ПЕРЕДАЛАЛ парсер ПО КАЙФУ ЖИ ЕС.
     Теперь он супер мега быстрый + качественный

--- Smart_Alarm/Parser.cs
+++ Smart_Alarm/Parser.cs
@@ -29,24 +29,15 @@ namespace Smart_Alarm
         {
             List<LessonJSON> lessons = new List<LessonJSON>();
             var doc = new HtmlWeb().Load(url);
-            var screen_reader_element = doc.DocumentNode.SelectSingleNode("//div[@class='timetable_wrapper']//table[@class='screen-reader-element']");
-            var days = screen_reader_element.SelectNodes("//tr").Where((num, index) => index % 2 == 0).ToList();
-
-            int count = 0;
+            var days = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[7]/div[2]/div[3]/div/div[1]/table[1]/tr").Where(trNode => trNode.InnerLength > 2000).ToList();
             foreach (var day in days)
             {
-                if (count < 6)
-                {
-                    var discipline = day.SelectSingleNode("//div[@class='hidden for_print']//span[@class='discipline'][text()]").InnerText.Trim();
-                    var auditoriums = day.SelectSingleNode("//span[@class='auditoriums'][text()]").InnerText.Trim();
-                    string[] data = new string[] { day.SelectSingleNode("//div[@class='modal-body']/p[2][text()]").InnerText.Split('\n')[2].Trim(),
-                    day.SelectSingleNode("//div[@class='modal-body']/p[3][text()]").InnerText.Split('\n')[2].Trim() };
-                    lessons.Add(new LessonJSON { Discipline = discipline, Auditorums = auditoriums, Date = data[0], Time = data[1] });
-                    day.RemoveAll();
-                }
-                else
-                    break;
-                ++count;
+                string disciplina = day.SelectSingleNode(".//div[1]/div/div[2]/span[1]").InnerText;
+                string auditoriya = day.SelectSingleNode(".//div[1]/div/div[2]/span[3]").InnerText;
+                string data_provedeniya = day.SelectSingleNode(".//div[2]/noindex/div/div/div/div[2]/p[2]").InnerText.Substring(30).Trim();
+                string time = day.SelectSingleNode(".//div[2]/noindex/div/div/div/div[2]/p[3]").InnerText.Substring(34).Trim();
+
+                lessons.Add(new LessonJSON { Discipline = disciplina, Auditorums = auditoriya, Date = data_provedeniya, Time = time });
             }
             return lessons;
         }
