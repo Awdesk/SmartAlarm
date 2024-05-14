04ff44757dde53d90d66abb663cf99f2b9968a25
SuperGun158 <tihomirov2005@gmail.com>
2024-05-14 17:57:47 +0700

    Revert "Версия будильника 1.0"
    This reverts commit 5580ffda0f7286b05f0b969928f63b848af44c93.

--- Smart_Alarm/Alarm/Alarm.cs
+++ Smart_Alarm/Alarm/Alarm.cs
@@ -1,39 +1,16 @@
-﻿using System;
+﻿using Android.Media;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Android.Media;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Util;
 
 namespace Smart_Alarm.Alarm
 {
-    [BroadcastReceiver]
-    public class Alarm: BroadcastReceiver
+    public class Alarm
     {
-        public string Name { get; set; }
+        public string Name {  get; set; }
         public string Description { get; set; }
         public DateTime DateTime { get; set; }
         public MediaPlayer Ringtone { get; set; }
         public bool IsToggled { get; set; } = true;
-        public override void OnReceive(Context context, Intent intent)
-        {
-            
-        }
-        public class AlarmService
-        {
-            public void SetRepeatingAlarm(Context context)
-            {
-                AlarmManager alarmManager = context.GetSystemService(Context.AlarmService) as AlarmManager;
-                Intent alarmIntent = new Intent(context, typeof(Alarm));
-                PendingIntent pendingIntent = PendingIntent.GetBroadcast(context, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
-
-                // Устанавливаем будильник на повторение каждые 10 минут
-                alarmManager.SetRepeating(AlarmType.RtcWakeup, DateTime.Now.Millisecond, 10 * 60 * 1000, pendingIntent);
-            }
-        }
-
     }
-
 }
